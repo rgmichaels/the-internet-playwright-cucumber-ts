a742da9e25a2caf9da340657c2742d6968c922a2
rgmichaels <95025342+rgmichaels@users.noreply.github.com>
2026-01-21 13:04:02 -0500

    Added test for text on status code page

--- features/42_status_codes.feature
+++ features/42_status_codes.feature
@@ -7,6 +7,11 @@ Feature: Status Codes
     Then the Status Codes page should load
     And each status code link should show the correct explanation
 
+
+  Scenario: Status Codes page show paragraph of text
+    Given I open the Status Codes page
+    Then the Status Codes page should show a paragraph of text
+
   Scenario: Status Codes page shows Elemental Selenium attribution in the footer
     Given I open the Status Codes page
     Then the global footer should be valid
--- src/steps/statusCodes.steps.ts
+++ src/steps/statusCodes.steps.ts
@@ -1,6 +1,7 @@
 import { Given, Then } from '@cucumber/cucumber';
 import { CustomWorld } from '../support/world';
 import { StatusCodesPage } from '../pages/StatusCodesPage';
+import { expect } from '@playwright/test';
 
 Given('I open the Status Codes page', async function (this: CustomWorld) {
   const po = new StatusCodesPage(this.page);
@@ -22,5 +23,21 @@ Then(
   'each status code link should show the correct explanation',
   async function () {
     await this.statusCodesPage.exercise();
+  }
+);
+
+Then(
+  'the Status Codes page should show a paragraph of text',
+  async function () {
+    const expectedText =
+      'HTTP status codes are a standard set of numbers used to communicate from a web server to your browser to indicate the outcome of the request being made';
+
+    // Locate the paragraph containing the explanation
+    const paragraph = this.page.locator('p').filter({
+      hasText: 'HTTP status codes are a standard set of numbers',
+    });
+
+    await expect(paragraph).toBeVisible();
+    await expect(paragraph).toContainText(expectedText);
   }
 );
